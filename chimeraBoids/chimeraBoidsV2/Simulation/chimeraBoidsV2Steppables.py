72255228870a09c26d3e77cb28578a35e4c0ea2d
juliano lab pc <julianoferrarigianlupi@gmail.com>
2019-03-07 10:58:33 -0500

    organizing the code

--- chimeraBoids/chimeraBoidsV2/Simulation/chimeraBoidsV2Steppables.py
+++ chimeraBoids/chimeraBoidsV2/Simulation/chimeraBoidsV2Steppables.py
@@ -23,12 +23,12 @@ global G_deltaBoids_G
 #parameter scan variables
 G_targetVolume_G = 64.
 G_lambdaVolume_G = 8.
-G_forceModulus_G = -20
+G_forceModulus_G = -20 #speeds are tipically 1/100 of force.
 G_deltaTime_G = 10
 
 G_alphaBoids_G = 5.5
 G_betaBoids_G = 1.5
-G_gammaBoids_G = .5
+G_gammaBoids_G = .1
 G_deltaBoids_G = .1
 
 
@@ -56,6 +56,7 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
     def start(self):
         # any code in the start function runs before MCS=0
         
+        
         #constants
         self.targetVolume = G_targetVolume_G
         self.lambdaVolume = G_lambdaVolume_G
@@ -67,6 +68,8 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
         self.betaBoids = G_betaBoids_G
         self.gammaBoids = G_gammaBoids_G
         self.deltaBoids = G_deltaBoids_G
+        #create data folder and write initial things
+        self.createWritingLoc()
         
         #set of used cluster IDs
         self.usedClusterIDs = set([0])#I think that to avoid possible errors it's best if the set is not
@@ -96,6 +99,9 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
                 cell.dict['velocityX_deltaT'] = 0.
                 cell.dict['velocityY_deltaT'] = 0.
                 
+                cell.dict['mean_neig_velX'] = 0
+                cell.dict['mean_neig_velY'] = 0
+                
                 cell.dict['previousForceX'] = self.forceModulus*np.cos(cell.dict['forceAngle']) 
                 cell.dict['previousForceY'] = self.forceModulus*np.sin(cell.dict['forceAngle']) 
                 
@@ -121,7 +127,64 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
                 
                 
     
-
+    def createWritingLoc(self):
+        #write location
+        
+        self.saveLoc = os.path.dirname(os.path.abspath(__file__))
+        self.saveLoc = os.path.join(self.saveLoc,'data')
+        if not os.path.exists(self.saveLoc):
+            os.makedirs(self.saveLoc)
+        
+        self.instVelFile = os.path.join(self.saveLoc,'meanInstantVelocity.dat')
+        self.dtVelFile = os.path.join(self.saveLoc,'meanDeltaTVelocity.dat')
+        
+        
+        
+        with open(os.path.join(self.saveLoc,'parameters.dat'),'w+') as paramFile:
+            paramFile.write('targetVolume = '+ str(self.targetVolume) +
+                            '\n lambdaVolume = ' + str(self.lambdaVolume) +
+                            '\n forceModulus = ' + str(self.forceModulus) +
+                            '\n deltaTime = ' + str(self.deltaTime) +
+                            '\n alphaBoids =' + str(self.alphaBoids) +
+                            '\n betaBoids = ' + str(self.betaBoids) +
+                            '\n gammaBoids = ' + str(self.gammaBoids) +
+                            '\n deltaBoids = ' + str(self.deltaBoids))
+    def writeData(self,mcs):
+        #mean total velocity 
+        
+        instVelsX = []
+        instVelsY = []
+        
+        dtVelsX = []
+        dtVelsY = []
+        for cell in self.cellList:
+            instVelsX.append(cell.dict['velocityX_instant'])
+            instVelsY.append(cell.dict['velocityY_instant'])
+            
+            if mcs>self.deltaTime:
+                dtVelsX.append(cell.dict['velocityX_deltaT'])
+                dtVelsY.append(cell.dict['velocityY_deltaT'])
+                
+        
+        with open(self.instVelFile, 'a+') as ivf:
+            mivx = np.mean(instVelsX)
+            eivx = np.std(instVelsX)
+            
+            mivy = np.mean(instVelsY)
+            eivy = np.std(instVelsY)
+            
+            ivf.write('%i,%f,%f,%f,%f\n'%(mcs, mivx,eivx, mivy,eivy))
+        
+        if mcs>self.deltaTime:
+            with open(self.dtVelFile, 'a+') as tvf:
+                mtvx = np.mean(dtVelsX)
+                etvx = np.std(dtVelsX)
+                
+                mtvy = np.mean(dtVelsY)
+                etvy = np.std(dtVelsY)
+                
+                tvf.write('%i,%f,%f,%f,%f\n'%(mcs, mtvx,etvx, mtvy,etvy))
+        return
     def positionTracking(self,mcs,cur_Cell):
         self.positionX = cur_Cell.dict['positionX']
         self.positionY = cur_Cell.dict['positionY']
@@ -161,19 +224,30 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
         neigVxList = np.array([])
         neigVyList = np.array([])
         
+        if len(self.getCellNeighborDataList(cur_Cell)) <=1:
+            #no neighbors, return 0. (medium is counted as neighbor, so <=1 not ==0)
+            cur_Cell.dict['mean_neig_velX'] = 0
+            cur_Cell.dict['mean_neig_velY'] = 0
+            return 0, 0
+        
+        
         for neighbor, commonSurfaceArea in self.getCellNeighborDataList(cur_Cell):
             if neighbor:
                 neigVxList = np.append(neigVxList,neighbor.dict['velocityX_deltaT'])
                 neigVyList = np.append(neigVyList,neighbor.dict['velocityY_deltaT']) 
         
-        #taking care of empty lists
+        #taking care of empty lists, just in case something goes wrong
         if len(neigVxList):
             neigVx = np.mean(neigVxList)
-        else:       
+            cur_Cell.dict['mean_neig_velX'] = neigVx
+        else:  
+            cur_Cell.dict['mean_neig_velX'] = 0      
             neigVx = 0
         if len(neigVyList):
             neigVy = np.mean(neigVyList)
+            cur_Cell.dict['mean_neig_velY'] = neigVy
         else:
+            cur_Cell.dict['mean_neig_velY'] = 0
             neigVy = 0
         
         return neigVx, neigVy
@@ -223,142 +297,7 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
         if len(set(neigsIDs)) > 1 and sorted(neigsIDs) not in self.equivalentIDs:
             self.equivalentIDs.append(sorted(neigsIDs))
     
-    
-    def reassingDoubleIDs(self,curr_Cell,eqs):
-        if curr_Cell.dict['clusterID'] == None:
-            return#
-        for e in eqs:
-                for notSmalestID in e[1:]:
-                    if curr_Cell.dict['clusterID'] == notSmalestID:
-                        curr_Cell.dict['clusterID'] = e[0]
-                        #print 'reasigned', notSmalestID, 'to', e[0]
-                        return
-    
-    def coloringByID(self,inUseClusterIDs):
-        
-        color = 0
-        if len(inUseClusterIDs)>0:
-            maxID = max(inUseClusterIDs)
-            inUseClusterIDs = sorted(list(set(inUseClusterIDs)))
-            interval = float(maxID)/len(inUseClusterIDs)
-            print maxID, len(inUseClusterIDs), interval
-            for cell in self.cellList:
-#                 print color
-                for i, id in enumerate(inUseClusterIDs):
-                    if cell.dict['clusterID'] == id:
-                        color = .5*((i+1)*interval)+.5*maxID
-                        cell.dict['color'] = color
-                        break
-    
-    def updateFields(self,mcs):
-        
-        ##for better colors in the cluster IDs I'll set them to be a % of the max ID
-        ##(previouslly the ID itself was used as color
-        for cCell in self.cellList:
-            
-            #updating scalar field for cluster coloring.
-            if cCell.dict['color'] != None:
-                self.scalarClusterIDfield[cCell] = cCell.dict['color']
-            else:
-                self.scalarClusterIDfield[cCell] = 0
-            
-            #updating scalar field for intantaneous angle of movement:
-            self.scalarAngleField[cCell] = 180*(cCell.dict['angle']+np.pi)/np.pi
-            #updating vector field for intantaneous velocity:
-            self.vectorIVelocityField[cCell] = [cCell.dict['velocityX_instant'], cCell.dict['velocityY_instant'], 0]
-            
-            if mcs>self.deltaTime:
-                self.vectorTVelocityField[cCell] = [cCell.dict['velocityX_deltaT'], cCell.dict['velocityY_deltaT'], 0]
-                self.vectorForceField[cCell] = [cCell.dict['previousForceX'], cCell.dict['previousForceY'], 0]
-        
-#         self.vectorTVelocityField = self.createVectorFieldCellLevelPy('dt_Velocity')
-        
-#         self.vectorForceField = self.createVectorFieldCellLevelPy("Force")
-    
-    def step(self,mcs):        
-        #type here the code that will run every _frequency MCS
-        
-        #############################################################
-        #first cell loop to update tracking of position, velocities, id assignment
-        
-        self.equivalentIDs = []
-        #first loop for id assignment
-
-        for cell in self.cellList:
-            cell.dict['color'] = None
-            self.assignClusterIDs(cell)
-            self.positionTracking(mcs,cell)
-            
-            
-            
-            if mcs > self.deltaTime:
-                vx = ( cell.dict['positionX'][-1] - cell.dict['positionX'][-self.deltaTime-1] )
-                vx = vx/self.deltaTime
-                
-                vy = ( cell.dict['positionY'][-1] - cell.dict['positionY'][-self.deltaTime-1] )
-                vy = vy/self.deltaTime
-                
-                cell.dict['velocityX_deltaT'] = vx
-                cell.dict['velocityY_deltaT'] = vy
-        ##############################################################
-        
-        #####################################################
-        
-        ##I need to identify the clusters. The most straitforward way I can think is
-        ## simply looping the cells, checking the neighbors and assingning a colective ID. If the
-        ## neighbor already has an ID use that one instead. This is what's done in the 1st loop.
-        ## Of course, this may lead to a single
-        ## cluster having multiple IDs, so a second pass will be necessary. 
-        ## there's also the issue of tracking the used IDs (witch will only really be an issue if 
-        ## the number of used IDs gets to be close to 2**32, I guess).
-        ########
-        
-        
-        
-        
-        for cell in self.cellList:
-            self.reassingDoubleIDs(cell,self.equivalentIDs)
-        
-        
-        #################################################
-        
-        
-        # cell loop to calculate the boid's force 
-        if mcs > self.deltaTime:
-            for cell in self.cellList:
-                
-                cellVx = cell.dict['velocityX_deltaT']
-                cellVy = cell.dict['velocityY_deltaT']
-                
-                #getting mean velocities of neighbours
-                neigVx, neigVy = self.getNeighboursMeanVelocity(cell)
-                
-                #calculating new force in x and y
-                forceX = self.calculateForce(cellVx,neigVx,cell.dict['previousForceX'])
-                forceY = self.calculateForce(cellVy,neigVy,cell.dict['previousForceY'])
-                
-                #getting the angle of new force
-                # # not sure about the implementation of noise
-                cell.dict['forceAngle'] = np.arctan2(forceY,forceX) + self.gammaBoids * np.random.uniform(-np.pi,np.pi)
-                
-                #aplying standard force with new angle
-                cell.lambdaVecX = self.forceModulus*np.cos(cell.dict['forceAngle']) 
-                cell.lambdaVecY = self.forceModulus*np.sin(cell.dict['forceAngle'])  
-                
-                cell.dict['previousForceX'] = forceX
-                cell.dict['previousForceY'] = forceY 
-#        
-        
-        inUseClusterIDs = []##will use this to assign colors
-        for cell in self.cellList:
-            if cell.dict['clusterID'] != None:
-                inUseClusterIDs.append(cell.dict['clusterID'])
-        
-        self.coloringByID(inUseClusterIDs)
-        self.currentIDs = set(inUseClusterIDs)
-        #updating extra fields
-        self.updateFields(mcs)
-        
+    def clusterBreakingDetection(self):
         
         ## there's the issue of a cluster breaking in
         ## two clusters, detecting that and spliting the ID.
@@ -418,10 +357,176 @@ class chimeraBoidsV2Steppable(SteppableBasePy):
         
         for cell in self.cellList:
             self.reassingDoubleIDs(cell,self.equivalencyToOld)
+    
+    def reassingDoubleIDs(self,curr_Cell,eqs):
+        if curr_Cell.dict['clusterID'] == None:
+            return#
+        for e in eqs:
+                for notSmalestID in e[1:]:
+                    if curr_Cell.dict['clusterID'] == notSmalestID:
+                        curr_Cell.dict['clusterID'] = e[0]
+                        #print 'reasigned', notSmalestID, 'to', e[0]
+                        return
+    
+    def coloringByID(self,inUseClusterIDs):
+        
+        color = 0
+        if len(inUseClusterIDs)>0:
+            maxID = max(inUseClusterIDs)
+            inUseClusterIDs = sorted(list(set(inUseClusterIDs)))
+            interval = float(maxID)/len(inUseClusterIDs)
+            print maxID, len(inUseClusterIDs), interval
+            for cell in self.cellList:
+#                 print color
+                for i, id in enumerate(inUseClusterIDs):
+                    if cell.dict['clusterID'] == id:
+                        color = .5*((i+1)*interval)+.5*maxID
+                        cell.dict['color'] = color
+                        break
+    
+    def updateFields(self,mcs):
+        
+        ##for better colors in the cluster IDs I'll set them to be a % of the max ID
+        ##(previouslly the ID itself was used as color
+        for cCell in self.cellList:
+            
+            #updating scalar field for cluster coloring.
+            if cCell.dict['color'] != None:
+                self.scalarClusterIDfield[cCell] = cCell.dict['color']
+            else:
+                self.scalarClusterIDfield[cCell] = 0
+            
+            #updating scalar field for intantaneous angle of movement:
+            self.scalarAngleField[cCell] = 180*(cCell.dict['angle']+np.pi)/np.pi
+            #updating vector field for intantaneous velocity:
+            self.vectorIVelocityField[cCell] = [cCell.dict['velocityX_instant'], cCell.dict['velocityY_instant'], 0]
+            
+            if mcs>self.deltaTime:
+                self.vectorTVelocityField[cCell] = [cCell.dict['velocityX_deltaT'], cCell.dict['velocityY_deltaT'], 0]
+                self.vectorForceField[cCell] = [cCell.dict['previousForceX'], cCell.dict['previousForceY'], 0]
+        
+#         self.vectorTVelocityField = self.createVectorFieldCellLevelPy('dt_Velocity')
+        
+#         self.vectorForceField = self.createVectorFieldCellLevelPy("Force")
+    
+    
+        
+    
+    
+    
+    
+    
+    def step(self,mcs):        
+        #type here the code that will run every _frequency MCS
         
         
+        #####################################################
+        
+        ##I need to identify the clusters. The most straitforward way I can think is
+        ## simply looping the cells, checking the neighbors and assingning a colective ID. If the
+        ## neighbor already has an ID use that one instead. This is what's done in the 1st loop.
+        ## Of course, this may lead to a single
+        ## cluster having multiple IDs, so a second pass will be necessary. 
+        ## there's also the issue of tracking the used IDs (witch will only really be an issue if 
+        ## the number of used IDs gets to be close to 2**32, I guess).
+        ########
+        
+        #first cell loop to update tracking of position, velocities, id assignment
+        
+        self.equivalentIDs = []
+        #first loop for id assignment
+
+        for cell in self.cellList:
+            cell.dict['color'] = None
+            self.assignClusterIDs(cell)
+            self.positionTracking(mcs,cell)
+            
+            
+            
+            if mcs > self.deltaTime:
+                vx = ( cell.dict['positionX'][-1] - cell.dict['positionX'][-self.deltaTime-1] )
+                vx = vx/self.deltaTime
+                
+                vy = ( cell.dict['positionY'][-1] - cell.dict['positionY'][-self.deltaTime-1] )
+                vy = vy/self.deltaTime
+                
+                cell.dict['velocityX_deltaT'] = vx
+                cell.dict['velocityY_deltaT'] = vy
+        
+        for cell in self.cellList:
+            self.reassingDoubleIDs(cell,self.equivalentIDs)
         
         
+        #################################################
+        
+        ## there's the issue of a cluster breaking in
+        ## two clusters, detecting that and spliting the ID.
+        ## 
+        ## I could keep track of the neighbors of every cell. Then, if the neighborhood
+        ## has changed check if the cell that left still has some neighbors. If this is so
+        ## then I can just assign a new ID to those that left. I'll do this check after the reasignment.
+        ## I'll have to delete the list of equivalencies before entering this function.
+        ## I also need a dictionary for the neighbors, probably the neighbors IDs. 
+        ## Also, I only care if the neigborhood has diminished.
+        ##
+        ## well, that didn't work
+        ##
+        ## Maybe I could reset the whole cluster ID thing every MCS. To keep history I could have the old 
+        ## ID in a dict. Then assing that one over the new one. But before that do the whole reassingDoubleIDs
+        ## and then have some kind of method for the separation. something like, if 2 cells have the same old ID 
+        ## only one of them gets to keep it.
+        ## 
+        ## right now it works, but it takes a couple of mcs to work. Well, latest test has it working imediatly.
+        ## so probably there was some glitch before. idk
+        
+        
+        self.clusterBreakingDetection()
+        
+        #################################################
+        # cell loop to calculate the boid's force 
+        if mcs > self.deltaTime:
+            for cell in self.cellList:
+                
+                cellVx = cell.dict['velocityX_deltaT']
+                cellVy = cell.dict['velocityY_deltaT']
+                
+                #getting mean velocities of neighbours
+                neigVx, neigVy = self.getNeighboursMeanVelocity(cell)
+                
+                #calculating new force in x and y
+                forceX = self.calculateForce(cellVx,neigVx,cell.dict['previousForceX'])
+                forceY = self.calculateForce(cellVy,neigVy,cell.dict['previousForceY'])
+                
+                #getting the angle of new force
+                # # not sure about the implementation of noise
+                cell.dict['forceAngle'] = np.arctan2(forceY,forceX) + self.gammaBoids * np.random.uniform(-np.pi,np.pi)
+                
+                #aplying standard force with new angle
+                cell.lambdaVecX = self.forceModulus*np.cos(cell.dict['forceAngle']) 
+                cell.lambdaVecY = self.forceModulus*np.sin(cell.dict['forceAngle'])  
+                
+                cell.dict['previousForceX'] = forceX
+                cell.dict['previousForceY'] = forceY 
+#        
+        
+        #################################################
+        
+        self.writeData(mcs)
+        
+        #################################################
+        inUseClusterIDs = []##will use this to assign colors
+        for cell in self.cellList:
+            if cell.dict['clusterID'] != None:
+                inUseClusterIDs.append(cell.dict['clusterID'])
+        
+        self.coloringByID(inUseClusterIDs)
+        self.currentIDs = set(inUseClusterIDs)
+        #updating extra fields
+        self.updateFields(mcs)
+        
+        
+
+        #################################################
         #cleanup
         print '!!!!!!!!!!!!'
         print self.currentIDs
